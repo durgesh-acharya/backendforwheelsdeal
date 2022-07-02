db0f9196dc354eb477c90c82a0617fe21de62c95
durgesh <durgesh.acharya2006@gmail.com>
2022-07-02 17:35:56 +0530

    cp by id added

--- routes/cp.js
+++ routes/cp.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
+//fetch all channel partner
+
 router.get('/cp', function(req, res, next) {
 
     const sql = "SELECT * FROM cp";
@@ -17,5 +19,29 @@ router.get('/cp', function(req, res, next) {
       res.json([{status : true, data : rows, msg : "cp retrived successfully!"}])
     })
   });
+
+  //fetch channel partner by id
+
+  router.get('/cp/byid/:id', function(req, res, next) {
+    const id = req.params.id;
+    
+    const sql = `SELECT * FROM cp WHERE associateid = ${id}`;
+    db.query(sql, function(err, row, fields) {
+      if (err) {
+        res.status(500).send({ error: 'Something failed!' })
+      }
+      if(row.length > 0){
+        res.setHeader('Content-Type', 'application/json');
+        res.json([{status : true, data : row[0], msg : "cp retrived successfully!"}])
+      }else{
+        res.json([{status : false, msg : "No cp Found!"}])
+      }
+     
+    })
+    });
+    
+
+
+
 
   module.exports = router;
